5a159141bf5ab6f3ebf924fbc238c124ba592314
John Park <spacecwby417@gmail.com>
2021-01-04 13:54:31 -0500

    Add most recent repos to the user profile carfd

--- script.js
+++ script.js
@@ -9,12 +9,25 @@ async function getUser(username) {
         const { data } = await axios(APIURL + username)
 
         createUserCard(data);
+        getRepos(username)
     } catch(err) {
         if(err.response.status == 404) {
             createErrorcard('No profile with this user name')
         }
     }   
 }
+
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created')
+
+        addReposToCard(data);
+    } catch(err) {
+        createErrorcard('Problem fetching repos')
+    }   
+}
+
 
 function createUserCard(user) {
     const cardHTML = `
@@ -50,6 +63,20 @@ function createErrorcard(msg) {
 
     main.innerHTML = cardHTML
 }
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos') 
+
+    repos.slice(0, 10).forEach(repo => {
+        const repoEl = document.createElement('a')
+        repoEl.classList.add('repo')
+        repoEl.href = repo.html_url
+        repoEl.target = '_blank'
+        repoEl.innerText = repo.name
+        reposEl.appendChild(repoEl)
+    })
+}
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
